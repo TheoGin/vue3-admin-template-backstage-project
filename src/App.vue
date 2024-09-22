63aa48336141d93615a2dc81d23212b4b9f321b1
Sophotheo <98071271+TheoGin@users.noreply.github.com>
2024-09-22 20:49:32 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
-<template>
-    
-</template>
+<template></template>
 
-<script setup lang="ts" >
-for(let i=0;i<100;i++){
-  console.log(i);
-  
+<script setup lang="ts">
+for (let i = 0; i < 100; i++) {
+  console.log(i)
 }
+
+let str = "本地格式化后加的一行代码";
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
