74786fdfe70517555409ce2281257e12503eba7d
Sophotheo <98071271+TheoGin@users.noreply.github.com>
2024-09-22 20:46:52 +0800

    提交格式化代码

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
-  <div class="box">
-    <h1>我是APP根组件</h1>
-  </div>
+    
 </template>
 
-<script setup lang="ts">
-const str = 'test'
+<script setup lang="ts" >
+for(let i=0;i<100;i++){
+  console.log(i);
+  
+}
 </script>
 
-<style scoped lang="scss">
-.box {
-  width: 400px;
-  height: 200px;
-  background-color: aquamarine;
-  h1 {
-    color: white;
-  }
-}
+<style scoped>
+
 </style>
