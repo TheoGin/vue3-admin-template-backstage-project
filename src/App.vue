1cafc014d462cde3f816caeb8f2aba20219f1ce8
Sophotheo <98071271+TheoGin@users.noreply.github.com>
2024-09-22 20:41:59 +0800

    提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>我是APP根组件</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = 'test'
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 400px;
+  height: 200px;
+  background-color: aquamarine;
+  h1 {
+    color: white;
+  }
+}
+</style>
